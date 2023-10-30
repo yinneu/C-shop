9b51e7ea30f123945071ce655dd5cbbc0e9c1e90
yinneu <yinneu@gmail.com>
2023-10-30 17:24:42 +0000

    start update shop api

--- static/js/shop.js
+++ static/js/shop.js
@@ -1,5 +1,7 @@
 var cardView = document.getElementById("modalCard");
 var cardViewEffect = document.getElementById("modalEffect");
+var cardViewPrice = document.getElementById("price"); //금액
+
 
 $('.stack').click(function(){
   $('#modal-container').removeAttr('class').addClass("two");
@@ -7,9 +9,9 @@ $('.stack').click(function(){
   
   cardView.children[0].style.backgroundImage = $(this).children(0).css('background-image');
   
-
   cardView.children[1].innerHTML = $(this).children()[0].outerHTML;
     cardViewEffect.innerText = $(this).children()[1].innerText;
+    cardViewPrice.innerText = 'x ' + $(this).children()[2].innerText;
   
 })
 
@@ -30,3 +32,9 @@ function scrollGrid() {
 	cards.style.setProperty("--scroll",transY + "%");
 }
 scrollGrid();
+
+
+//추가할 사항
+//구매 버튼 클릭시 로그인한 상태가 아니면 로그인
+//로그인 상태면 유저의 열매개수와 해당 구매권을 비교하고 구매가 불가능을 알려줌. (check를 날리기)
+//구매요청을 날리면 백에서 구매 여부 확인후 불가능이면 false, 가능이면 구매할것인지 확인 후
